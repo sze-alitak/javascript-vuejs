2f5c208b14bd2a467490b6adcf54c67bb4753059
alitak <kukel.attila@gmail.com>
2025-09-26 12:24:22 +0200

    define model bevezetése

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,32 +1,10 @@
 <script setup>
-
-
-
-
-
-
-
-
-
-
-
-import { defineProps, defineEmits } from 'vue'
-
-defineProps({
-    modelValue: String
-})
-
-const emit = defineEmits(['update:modelValue'])
-
-const add = (e) => {
-    emit('update:modelValue', e.target.value)
-}
+const modelValue = defineModel()
 </script>
 
 <template>
     <input
         class="border-1 border-gray-300 rounded mt-2 p-1 focus:bg-gray-200"
-        :value="modelValue"
-        v-on:change="add"
+        v-model="modelValue"
         type="text"/>
 </template>

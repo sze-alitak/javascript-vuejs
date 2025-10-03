bccbd6cfd845df15caed1757e4457d022cc93c16
alitak <kukel.attila@gmail.com>
2025-10-03 12:22:03 +0200

    BookTags cleanup vol.3

--- src/components/BookTags.vue
+++ src/components/BookTags.vue
@@ -3,16 +3,11 @@ import { computed } from 'vue'
 
 const props = defineProps({
     initialTags: Array,
-    modelValue: String,
 })
 
-const emit = defineEmits(['update:modelValue'])
+const activeTag = defineModel()
 
 const tags = computed(() => ["all", ...new Set(props.initialTags)])
-
-const selectTag = (tag) => {
-    emit('update:modelValue', tag)
-}
 </script>
 
 <template>
@@ -20,10 +15,10 @@ const selectTag = (tag) => {
         <button 
         v-for="tag in tags" 
         class="border-1 p-1 hover:bg-gray-400 cursor-pointer"
-        :class="{'bg-gray-200': props.modelValue === tag}"
+        :class="{'bg-gray-200': activeTag === tag}"
         :key="`tag-${tag}`"
         type="button"
-        v-on:click="selectTag(tag)"
+        v-on:click="activeTag = tag"
         >
             {{ tag }}
         </button>
